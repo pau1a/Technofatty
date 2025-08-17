7b622ab27b9fe4b0a2a132e334f3b91e05ac545d
Paula Livingstone <paula@paulalivingstone.com>
2025-08-17 07:49:37 +0000

    Revert "feat: Implement desktop navigation and sticky header"
    This reverts commit 5d45f83c8363cfcbe32cb4beab0fbac7c4dc248c.

--- coresite/views.py
+++ coresite/views.py
@@ -4,5 +4,5 @@ from .models import SiteImage
 
 def homepage(request):
     images = {img.key.replace("-", "_"): img for img in SiteImage.objects.all()}
-    context = {"site_images": images, "is_homepage": True}
+    context = {"site_images": images}
     return render(request, "coresite/homepage.html", context)
