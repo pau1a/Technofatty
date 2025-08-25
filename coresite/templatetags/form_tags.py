35982a0c5cdb3641c1409eb5595c7e6a4e30ad18
Paula Livingstone <paula@paulalivingstone.com>
2025-08-25 18:22:22 +0100

    Fix add_attrs tag and template attribute names

--- coresite/templatetags/form_tags.py
+++ coresite/templatetags/form_tags.py
@@ -4,5 +4,10 @@ register = template.Library()
 
 @register.simple_tag
 def add_attrs(field, **attrs):
-    """Render a form field with additional HTML attributes."""
-    return field.as_widget(attrs=attrs)
+    """Render a form field with additional HTML attributes.
+
+    Hyphenated attribute names can be passed using underscores in the
+    template, e.g. ``aria_describedby``.
+    """
+    normalized = {key.replace("_", "-"): value for key, value in attrs.items()}
+    return field.as_widget(attrs=normalized)
